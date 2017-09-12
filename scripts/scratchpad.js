af616d2a5146746402db53b7278603debb8a2d4e
ashley henry <ashley.henry@reedonline.co.uk>
2017-09-12 17:47:15 +0100

    init 0.1

---
+++ scripts/scratchpad.js
@@ -0,0 +1,19 @@
+(function() {
+  var x = "";
+  var y = "";
+  var adventure = {
+    "run":false,
+    "gameSiteLog": []
+  }
+
+    gameSiteLog[0] = "Welcome to super fun building site adventure javascript edition";
+    gameSiteLog[1] = "You need to set your Adventurer's attributes to begin";
+
+    adventure.run;
+
+    while(adventure.run === true){
+      for (var i = 0; i < gameSiteLog.array.length; i++) {
+        
+      }  
+    }
+})()
