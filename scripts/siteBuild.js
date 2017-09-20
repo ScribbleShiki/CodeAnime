c3cbc91152ba1b2c146b7069baeba01923ec8710
ashley henry <ashley.henry@reedonline.co.uk>
2017-09-20 14:21:43 +0100

    new intromodal rule

--- scripts/siteBuild.js
+++ scripts/siteBuild.js
@@ -1,9 +1,12 @@
 (function () {
     var questModal = document.getElementById("questModal");
+    var questManage = [questModal];
+
+    var questIntro = questManage[0];
 
     /* places a boundary for the content inside the page */
     var pageBoundary = $("#wrapper").click(function () {
-        pageBoundary.css('border', "3px solid red");
+        pageBoundary.css('border', "1px solid #000");
     });
 
     /* users will intereract with a modal type message interface */
@@ -12,6 +15,9 @@
         modalBox.setAttribute("id", "questModal");
         document.getElementById("questWrapper").appendChild(modalBox);
     });
+
+    /* A side bar will be displayed once the first modal is clicked (will have to remember user someday) */
+    questIntro.addEventListener("click", handler);
 
     /* if the modal exists give it a message */
     if($(questModal)){
