18249a9c9b628a2e6e988bf31b2cf0ba3c212dc1
ashley henry <ashley.henry@reedonline.co.uk>
2017-09-21 17:05:06 +0100

    added scrap file

--- scripts/main.js
+++ scripts/main.js
@@ -1,54 +1,55 @@
-(function () {
-  var _adventure = {
-    "run": false,
-    "gameQuestLog": [""],
-    "isValidUser": false
-  };
+var _adventure = {
+  "run": false,
+  "gameQuestLog": [""],
+  "isValidUser": false
+};
 
-  var _user = function (name, validName) {
-    name = this.name;
-    validName = this.validName;
+var _user = function (name, validName) {
+  name = this.name;
+  validName = this.validName;
 
-    var createNew = function() {
-      this.name = prompt("Please enter your name");
-      this.validName = function () {
-        if (_user.name !== "")
-          return true;
-        else
-          throw "please enter a name!";
-          return false;
-      };
-      
-      _adventure.isValidUser = true;
+  var createNew = function() {
+    this.name = prompt("Please enter your name");
+    this.validName = function () {
+      if (_user.name !== "")
+        return true;
+      else
+        throw "please enter a name!";
+        return false;
     };
+    
+    _adventure.isValidUser = true;
   };
+};
 
-  var _questModal = document.getElementById("questModal");
+var $questModal = $("#questModal");
+var _questManage = [$questModal];
+var _questIntro = _questManage[0];
 
-  /* adventure text file */
-  _adventure.gameQuestLog[0] = "Welcome to super fun building site adventure javascript edition";
-  _adventure.gameQuestLog[1] = "You need to set the site's basic attributes to begin";
+/* adventure text file */
+_adventure.gameQuestLog[0] = "Welcome to super fun building site adventure javascript edition";
+_adventure.gameQuestLog[1] = "You need to set the site's basic attributes to begin";
 
-  _adventure.run = true;
+_adventure.run = true;
 
-  /* will need to wait for user action before sending new text */
-  function generateQuestText() {
-    for (var i = 0; i < _adventure.gameQuestLog.length; i++) {
-      _questModal.innerHTML = _adventure.gameQuestLog[i];
-    }
+/* will need to wait for user action before sending new text */
+function generateQuestText() {
+  for (var i = 0; i < _adventure.gameQuestLog.length; i++) {
+    var questText = _adventure.gameQuestLog[i];
   }
+  return questText;
+}
 
-  /* may have to run async */
-  function addQuestText() {
-    _questModal.addEventListener('click', generateQuestText(), false);
-  }
+/* may have to run async */
+function addQuestText() {
+  console.log("fsdfa");
+  $("#questModal").html("hello");
+}
 
-  /* site loops through messages and conditions inside modal */
-  while (_adventure.run) {
-        /* once the modal exists give it a message */
-        if($(_questModal)){
-          addQuestText();
-        }
-    _adventure.run = false;
-  }
-})();
+/* site loops through messages and conditions inside modal */
+while (_adventure.run) {
+      /* once the modal exists give it a message */
+      if($questModal){
+        addQuestText();
+      }
+}
--- scripts/siteBuild.js
+++ scripts/siteBuild.js
@@ -1,21 +1,14 @@
-(function () {
-    var _questModal = document.getElementById("questModal");
-    
-    var _questManage = [_questModal];
-    var _questIntro = _questManage[0];
+var _questModal = document.getElementById("questModal");
 
-    /* users will intereract with a modal type message interface */
-    function newQuest() {
-        $("#wrapper").click(function buildQuestModal() {
-        var questModal = document.createElement("div");
-        questModal.setAttribute("id", "questModal");
-        questModal.setAttribute("class", "quest-arrows");
-        document.getElementById("questWrapper").appendChild(questModal);
-        });
-    }
+/* users will intereract with a modal type message interface */
+function newQuest() {
+    var questModal = document.createElement("div");
+    questModal.setAttribute("id", "questModal");
+    questModal.setAttribute("class", "quest-arrows");
+    document.getElementById("questWrapper").appendChild(questModal);
+}
 
-    /* A side bar will be displayed once the first modal is clicked (will have to remember user someday) */
-    _questIntro.addEventListener('click', function(){
+/* A side bar will be displayed once the first modal is clicked (will have to remember user someday) */
+/*_questIntro.addEventListener('click', function(){
 
-    });
-})();
+});*/
